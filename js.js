4eb8bca3e331df8c2663873e62097bcc340662cb
nawarajkharel9 <nawarajkharel9@gmail.com>
2026-07-04 16:01:10 +0545

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+const burger = document.getElementById("hamburger");
+const list = document.getElementById("navlist");
+
+
+burger.addEventListener("click", ()=>{
+    list.classList.toggle("navlist-active");
+})
